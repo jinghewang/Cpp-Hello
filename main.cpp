6e38127a0e3906e4d16eadf70ca90181b0f3542a
jinghewang <jinghewang@163.com>
2017-07-27 21:53:39 +0800

    const

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
+
+#define LENGTH 10;
+
+int func(int z);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    const int LENGTH2 = 10;
+
+    int a = 1;
+    int b = 2;
+    int z = func(b);
+    cout << "a+b=" << a+b << endl << "z:" << z << LENGTH;
     return 0;
+}
+
+int func(int z){
+    return z+1;
 }
