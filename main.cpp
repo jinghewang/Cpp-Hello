f706e3885c5f0c9eb38c336a4dcf35d6cdec669f
jinghewang <jinghewang@163.com>
2017-07-27 22:00:47 +0800

    file

--- main.cpp
+++ main.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 #define LENGTH 10;
 
 int func(int z);
+int test_file();
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    const int LENGTH2 = 10;
-
-    int a = 1;
-    int b = 2;
-    int z = func(b);
-    cout << "a+b=" << a+b << endl << "z:" << z << LENGTH;
+    int a = test_file();
+    cout << a << endl;
     return 0;
 }
 
 int func(int z){
     return z+1;
+}
+
+int test_file(){
+    char data[100];
+
+    // 以写模式打开文件
+    ofstream outfile;
+    outfile.open("afile.dat");
+
+    cout << "Writing to the file" << endl;
+    cout << "Enter your name: ";
+    cin.getline(data, 100);
+
+    // 向文件写入用户输入的数据
+    outfile << data << endl;
+
+    cout << "Enter your age: ";
+    cin >> data;
+    cin.ignore();
+
+    // 再次向文件写入用户输入的数据
+    outfile << data << endl;
+
+    // 关闭打开的文件
+    outfile.close();
+
+    // 以读模式打开文件
+    ifstream infile;
+    infile.open("afile.dat");
+
+    cout << "Reading from the file" << endl;
+    infile >> data;
+
+    // 在屏幕上写入数据
+    cout << data << endl;
+
+    // 再次从文件读取数据，并显示它
+    infile >> data;
+    cout << data << endl;
+
+    // 关闭打开的文件
+    infile.close();
 }
